a483a3c61297b12a15d6afc8004d5b36be859d18
buyyaa <buyyav20@gmail.com>
2025-02-01 16:34:38 -0800

    fixed the API 123555

--- utils/rmp-api.js
+++ utils/rmp-api.js
@@ -48,15 +48,42 @@ export async function searchProfessor(name, schoolId) {
     'Referer': 'https://www.ratemyprofessors.com/',
     'apollographql-client-name': 'rmp-web',
     'apollographql-client-version': '1.0.0',
-    'Cookie': 'ccpa-notice-viewed-02=true; trc_cookie_storage=taboola%2520global%253Auser-id%3D5ce11c44-1d00-45d0-af62-9ca1d888df77-tuctb4bc17'
+    'Authorization': 'Basic dGVzdDp0ZXN0',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'Cookie': 'ccpa-notice-viewed-02=true; trc_cookie_storage=taboola%2520global%253Auser-id%3D5ce11c44-1d00-45d0-af62-9ca1d888df77-tuctb4bc17; _ga=GA1.1.1849503966.1673632720; _ga_WE3KFFCPK9=GS1.1.1673632719.1.1.1673632739.0.0.0'
   };
 
   try {
+    // First, get the CSRF token
+    const tokenResponse = await axios.options(RMP_GRAPHQL_URL, {
+      headers: {
+        'Access-Control-Request-Method': 'POST',
+        'Access-Control-Request-Headers': 'content-type',
+        'Origin': 'https://www.ratemyprofessors.com'
+      }
+    });
+
+    // Add CSRF token if available
+    const csrfToken = tokenResponse.headers['x-csrf-token'];
+    if (csrfToken) {
+      headers['x-csrf-token'] = csrfToken;
+    }
+
     console.log('Request Headers:', headers);
     console.log('Request Body:', JSON.stringify(query, null, 2));
 
-    const response = await axios.post(RMP_GRAPHQL_URL, query, { headers });
+    const response = await axios.post(RMP_GRAPHQL_URL, query, { 
+      headers,
+      withCredentials: true
+    });
+    
     console.log('Response Status:', response.status);
+    console.log('Response Headers:', response.headers);
     console.log('Response Data:', JSON.stringify(response.data, null, 2));
 
     const professor = response.data?.data?.newSearch?.teachers?.edges[0]?.node;
